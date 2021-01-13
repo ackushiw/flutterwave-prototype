f77004aa6fa41e6f1fd011cdefeb9552a9a83015
ackushiw <ackw.kushi@gmail.com>
2021-01-13 18:59:15 -0500

    feat(styles): add fonts and basic theming

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,11 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['normalize.css/normalize.css'],
+  css: [
+    'normalize.css/normalize.css',
+    '~assets/styles/global.css',
+    '~assets/styles/theme.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
