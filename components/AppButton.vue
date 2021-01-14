33c7df3eb8fa0083b064045738900bf4af9f9799
ackushiw <ackw.kushi@gmail.com>
2021-01-13 19:00:25 -0500

    feat: create styled button

---
+++ components/AppButton.vue
@@ -0,0 +1,81 @@
+<template>
+  <NuxtLink v-if="to" :class="classNames" :to="to" v-on="$listeners">
+    <span class="center row">{{ label }}</span>
+  </NuxtLink>
+
+  <button v-else :class="classNames" v-on="$listeners">
+    <span class="center row">{{ label }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    dense: {
+      default: false,
+      type: Boolean,
+    },
+    label: {
+      required: true,
+      type: String,
+    },
+    primary: {
+      default: false,
+      type: Boolean,
+    },
+    to: {
+      default: null,
+      type: String,
+    },
+  },
+
+  computed: {
+    classNames() {
+      return [this.dense && 'dense', this.primary && 'theme--primary']
+    },
+  },
+}
+</script>
+
+<style scoped>
+button,
+a {
+  border-radius: 4px;
+  color: #333;
+  cursor: pointer;
+  display: block;
+  font-family: 'Flutterwave Bold', sans-serif;
+  font-size: 1.166rem;
+  font-weight: bold;
+  height: 50px;
+  padding: 12px 16px;
+  text-align: center;
+  text-decoration: none;
+}
+
+button.dense {
+  font-size: 1rem;
+
+  /* Design pixel perfect */
+  font-size: 12.2008px;
+  height: 38px;
+}
+
+a:not(.theme--primary):hover {
+  text-decoration: underline;
+}
+
+span {
+  height: 100%;
+}
+
+.theme--primary {
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+  text-transform: uppercase;
+}
+
+.theme--primary:hover {
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.5);
+  filter: hue-rotate(-10deg);
+}
+</style>
