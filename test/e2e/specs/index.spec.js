a900b65f6bc90ee9d6a2dd04d24535d3a3bea517
ackushiw <ackw.kushi@gmail.com>
2021-01-18 03:55:47 -0500

    fix(tests): add basic tests for homepage

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -1,8 +1,16 @@
 const Page = require('../pageObjects/main.page')
 
-describe('Example test', () => {
+describe('Homepage test', () => {
   it('should open correct app', () => {
     Page.open()
-    expect(browser).toHaveTitle('flutterwave-tickets')
+    expect(browser).toHaveTitle('Flutterwave events')
+  })
+
+  it('should have correct heading', () => {
+    Page.open()
+    const $heading = $('h1')
+
+    expect($heading).toBeDisplayed()
+    expect($heading).toHaveText('The best events happening now.')
   })
 })
